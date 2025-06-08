505deec16ace24f6e5cf60f6eb7871d7db0b93b1
sda_sami <sami@salondigiapu.fi>
2025-06-08 16:44:39 +0300

    Final2

--- main.py
+++ main.py
@@ -3,19 +3,16 @@ from stats import number_of_characters, sort_characters
 import sys
 
 def get_book_text(filepath): 
-    #file_contents = ""
     with open(filepath) as f:
         return f.read()
 
 
 def main():
     try:
-        #book_path = "books/frankenstein.txt"
         book_path = sys.argv[1]
         text = get_book_text(book_path)
         num_words = number_of_words(text)
         characters_and_numbers = number_of_characters(text)
-        #print(characters_and_numbers)
         sort_char_and_number = sort_characters(characters_and_numbers)
         report(book_path, sort_char_and_number, num_words)
 
@@ -23,18 +20,13 @@ def main():
         print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
         
-
     
-    
-
 def report(book_path, sort_char_and_number, num_words):
-    #print(argv[1])
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
-    #print(sort_char_and_number)
     for c in sort_char_and_number:
         if c['char'].isalpha():
             print(f"{c['char']}: {c['num']}")
--- stats.py
+++ stats.py
@@ -1,7 +1,6 @@
 def number_of_words(file_contents):
     num_words = 0
     return len(file_contents.split())
-    #print(f"Found {num_words} total words")
 
 # Oma koodi:
 """def number_of_characters(file_contents):
@@ -38,19 +37,15 @@ def number_of_characters(file_contents):
             word_list[lowered] += 1
         else:
             word_list[lowered] = 1
-    #print(word_list)
     return word_list
 
 def sort_on(dict):
     return dict["num"]
 
 def sort_characters(characters_and_numbers):
-    #print(characters_and_numbers)
     dictionary = []
-    dictionary_total = {}
     for c in characters_and_numbers:
         dictionary.append({"char": c, "num": characters_and_numbers[c]})
     dictionary.sort(reverse=True, key=sort_on)
     return dictionary
-    #print(dictionary)
 
